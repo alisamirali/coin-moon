935825ce9d8cd7894745668191a4edd712a5f3e6
Ali Samir <alisamir.eng@gmail.com>
2022-12-19 04:22:34 +0200

    Upload Project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { useEffect, useState } from "react";
+import "./App.css";
+import Coin from "./components/CoinItem/Coin";
 
 function App() {
+  const [coins, setCoins] = useState([]);
+  const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
+      )
+      .then((res) => {
+        setCoins(res.data);
+        // console.log(res.data);
+      })
+      .catch((error) => console.error(error));
+  }, []);
+
+  const handleChange = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const filteredCoins = coins.filter((coin) =>
+    coin.name.toLowerCase().includes(search.toLowerCase())
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+    <>
+      <div className="header">
+        <h1 className="brand">
+          <i className="fas fa-moon"></i> CoinMoon
+        </h1>
+        <form>
+          <input
+            className="inputField"
+            type="text"
+            onChange={handleChange}
+            placeholder="Search for coins"
+          />
+        </form>
+      </div>
+
+      <div className="coinsContainer">
+        {filteredCoins.map((coin, index) => {
+          return (
+            <Coin
+              key={coin.id}
+              name={coin.name}
+              price={coin.current_price}
+              symbol={coin.symbol}
+              marketcap={coin.market_cap}
+              volume={coin.total_volume}
+              image={coin.image}
+              priceChange={coin.price_change_percentage_24h}
+            />
+          );
+        })}
+      </div>
+
+      <p className="copyright">
+        Copyright @2022 .{" "}
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          href="https://ali-samir-dev.vercel.app/"
           target="_blank"
-          rel="noopener noreferrer"
+          rel="noreferrer"
         >
-          Learn React
+          Ali Samir
         </a>
-      </header>
-    </div>
+      </p>
+    </>
   );
 }
 
